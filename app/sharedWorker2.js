929892d6c97cc7fbecb416decdf9e928a431af01
Ayushman Chhabra <ayushmxn@outlook.com>
2022-08-05 17:48:50 -0400

    Issue 7905 repro

---
+++ app/sharedWorker2.js
@@ -0,0 +1,11 @@
+onconnect = function (e) {
+  const bc = new BroadcastChannel("bus");
+  bc.onmessage = (m) => {
+    console.log(m.data);
+  };
+  let i = 0;
+  setInterval(function () {
+    bc.postMessage("ping from worker 2" + i);
+    i += 1;
+  }, 2000);
+};
