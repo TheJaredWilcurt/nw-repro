c8874a4743ad1eb2ba119b0fd3331237b200dcf4
Ayushman Chhabra <ayushmxn@outlook.com>
2022-08-05 18:55:17 -0400

    simplified approach

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,10 @@
 const gulp = require("gulp");
-const NwBuilder = require("nw-builder");
-
-const nw = new NwBuilder({
-  files: ["package.json", "client//", "public//", "node_modules/"],
-  mode: "build",
-  version: "0.66.1",
-  flavor: "sdk",
-  zip: true,
-  platforms: ["win32", "win64", "osx64"],
-  buildType: "versioned",
-  buildDir: "./builds/sdk/", // Where the build version is saved
-  macIcns: "./public/images/ae.icns",
-});
+const { nwbuild } = require("nw-builder");
 
 gulp.task("build-dev", (done) => {
-  nw.options.flavor = "sdk";
-  nw.options.buildDir = "./builds/sdk/";
-  nw.build(() => {});
+  nwbuild({
+    files: "./app/**/*.*",
+    mode: "build",
+  });
   done();
 });
