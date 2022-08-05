da22d8cf8f0f4245cad5197272472a43a5a987d5
Ayushman Chhabra <ayushmxn@outlook.com>
2022-08-05 19:36:04 -0400

    set NW.js version to v0.66.1

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ const { nwbuild } = require("nw-builder");
 gulp.task("build-dev", (done) => {
   nwbuild({
     files: "./app/**/*.*",
+    version: "0.66.1",
     mode: "build",
+    platforms: ["osx64"]
   });
   done();
 });
